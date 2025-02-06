ce4992cf2836980735f98d8557b94886f473b96e
Mursalin_Leon <121357813+Mursalin95@users.noreply.github.com>
2025-02-07 02:22:25 +0600

    Update User1.cs

--- StockFusion/User1.cs
+++ StockFusion/User1.cs
@@ -1,1 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockFusion
+{
+    public partial class User1 : Form
+    {
+        public User1()
+        {
+            InitializeComponent();
+        }
+
+        private void roundedButton1_Click(object sender, EventArgs e)
+        {
+            SignUp signUp = new SignUp("Manager");
+            signUp.Show();
+            this.Hide();
+        }
+
+        private void roundedButton2_Click(object sender, EventArgs e)
+        {
+            SignUp signUp = new SignUp("FinancialManager");
+            signUp.Show();
+            this.Hide();
+        }
+    }
+}
 
